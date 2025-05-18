02d39754075aa4cc54f17a49230635142e64ea39
balu <demo@gmail.com>
2025-05-18 07:48:07 +0530

    image size increased

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ export default function App() {
   const[angle,setAngle]=useState(0)
   const style={
      display:'inline-block',
-     transition:'transform 10.5s ease-in-out',
+     transition:'transform 0.5s ease-in-out',
      transform:`rotate(${angle}deg)`
     }
   
   return (
     <div>
       <span  style={style} onClick={()=>setAngle(p=>p+360)}>
-        <img src={myImage} alt='life' style={{borderRadius:'8px', height:"100px", width:"100px"}} />
+        <img src={myImage} alt='life' style={{borderRadius:'50%', height:"400px", width:"400px"}} />
       </span>
     </div>
   );
